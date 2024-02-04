acfb9bc80510adbd73f95a2a35ef7fa737c3c6f9
jesicasusanto <jesica.ssnt@gmail.com>
2024-02-04 01:26:21 -0500

    Add lifespan function to populate the database when the server starts

--- stations/stations_service.py
+++ stations/stations_service.py
@@ -4,7 +4,41 @@ from pydantic import BaseModel
 import models
 from database import engine, Sessionlocal
 from sqlalchemy.orm import Session
-app = FastAPI()
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    db = Sessionlocal()
+    num_stations = db.query(models.Stations).count()
+    if num_stations == 0:
+        stations = [
+            ("UTM Bus Station", "Mississauga, ON L5L 1C6", 43.547852, -79.663229),
+            ("Student Centre Station", "3359 Mississauga Rd, Mississauga, ON L5L 1C6", 43.549030, -79.663554),
+            ("CCT Station", "Mississauga, ON L5L 1J7", 43.549449, -79.663100),
+            ("MN Station", "Mississauga, ON L5L 1J7", 43.550708, -79.663328),
+            ("Dean Henderson Memorial Park Station", "Mississauga, ON L5K 2R1",43.533203, -79.659932),
+            ("Sir Johns Homestead Station", "3061 Sir Johns Homestead #29, Mississauga, ON L5L 2N4", 43.541112, -79.662074),
+            ("Erindale Park Station", "1560 Dundas St W, Mississauga, ON L5C 1E5", 43.545935, -79.652739),
+            ("Woodchester Station", "2605 Woodchester Dr, Mississauga, ON", 43.526129, -79.675644),
+            ("Collegeway Station", "2686 The Collegeway #101, Mississauga, ON L5L 2M9 ", 43.531145, -79.692270),
+            ("Central Pkwy Station", "1132 Central Pkwy W, Mississauga, ON L5C 4E5 ",43.566689, -79.659101),
+            ("Square One Station", "2800 Lawrences, Mississauga, ON L5L 2N5", 43.594678, -79.644202),
+        ]
+        for station_info in stations:
+            station_model = models.Stations(
+                name=station_info[0],
+                address=station_info[1],
+                x=station_info[2],
+                y=station_info[3]
+            )
+            db.add(station_model)
+            db.commit()
+        yield()
+    else:
+        print(f"Found {num_stations} stations in the database. Skipping population.")
+        yield()
+
+app = FastAPI(lifespan=lifespan)
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -32,14 +66,14 @@ def read_api(db: Session = Depends(get_db)):
     """
     return db.query(models.Stations).all()
 
-app.get("/{station_id}")
+@app.get("/stations/{station_id}")
 def get_station(station_id:int,db: Session = Depends(get_db)):
     """
     A function to get a station with a database session as a parameter and return the station with the given ID.
     """
     return db.query(models.Stations).filter(models.Stations.id == station_id).first()
 
-@app.post("/")
+@app.post("/stations")
 def create_station(station: Station, db: Session = Depends(get_db)):
     """
     Create a station in the database.
@@ -56,7 +90,7 @@ def create_station(station: Station, db: Session = Depends(get_db)):
     db.commit()
     return station
 
-@app.put("/{station_id}")
+@app.put("/stations")
 def update_station(station_id:int, station: Station, db: Session = Depends(get_db)):
     """
     Update a station in the database with the given station_id. 
@@ -81,7 +115,7 @@ def update_station(station_id:int, station: Station, db: Session = Depends(get_d
     db.commit()
     return station
 
-@app.delete("/{station_id}")
+@app.delete("/stations")
 def delete_station(station_id:int, db: Session = Depends(get_db)):
     """
     Deletes a station with the given station ID from the database.
@@ -100,4 +134,4 @@ def delete_station(station_id:int, db: Session = Depends(get_db)):
     db.commit()
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
