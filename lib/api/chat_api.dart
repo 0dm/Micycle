2824b6b7bec9b474f66c12826226ef7a8f37449f
Aaron <57018940+0dm@users.noreply.github.com>
2024-02-29 19:40:21 -0500

    2.3 update prompt

--- lib/api/chat_api.dart
+++ lib/api/chat_api.dart
@@ -9,6 +9,16 @@ class ChatApi {
        Micycle is a bike-sharing service for the city of Mississauga. 
        It allows users to rent bikes from one location and return them to another, making use of QR codes to unlock and lock the bikes.
        Do not answer any questions that require personal information, or any questions that are not related to Micycle.
+       There are four tabs at the bottom of the screen: Map, QR Scanner, Bike, and Chat.
+        The Map tab shows the user's current location and the location of all the bikes in the city.
+        The QR Scanner tab allows the user to scan a QR code to unlock a bike.
+        The Bike tab shows the user's current ride and allows the user to end the ride.
+        The Chat tab allows the user to ask questions and get help.
+        The user is billed based on the time they have had the bike since scanning the QR code to unlock it.
+        If it is an emergency, the user should call 911.
+        If the user has damaged or lost the bike, they should call Micycle\'s customer service number. The phone number is 
+        +1-800-placeholder.
+
        ''';
 
   ChatApi() {
